vanessa2808/dn_oe36_fdf

notification-email-order-to-admin

app/Repositories/Eloquent/OrderRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Eloquent;
 
+use App\Jobs\SendEmailToUser;
 use App\Models\Cart;
 use App\Models\Order;
 use App\Models\OrderDetail;
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Mockery\Exception;
+use Illuminate\Http\Request;
+use Mail;
 
 class OrderRepository extends BaseRepository implements OrderRepositoryInterface
 {

tests/Feature/Mail/MailTest.php
@@ -1,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Mail;
+
+use Tests\TestCase;
+use App\Jobs\SendEmailToUser;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+
+class MailTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_send_mail_order()
+    {
+        Queue::fake();
+
+        SendEmailToUser::dispatch('yenrion9941@gmail.com', [], 200);
+
+        Queue::assertPushed(SendEmailToUser::class);
+    }
+}

vanessa2808/ecomercial

app/Repositories/Eloquent/OrderRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Eloquent;
 
+use App\Jobs\SendEmailToAdmin;
 use App\Models\Cart;
 use App\Models\Order;
 use App\Models\OrderDetail;
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Mockery\Exception;
+use Illuminate\Http\Request;
+use Mail;
 
 class OrderRepository extends BaseRepository implements OrderRepositoryInterface
 {

tests/Feature/Mail/MailTest.php
@@ -1,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Mail;
+
+use Tests\TestCase;
+use App\Jobs\SendEmailToAdmin;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+
+class MailTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_send_mail_order()
+    {
+        Queue::fake();
+
+        SendEmailToAdmin::dispatch('yenrion9941@gmail.com', [], 200);
+
+        Queue::assertPushed(SendEmailToAdmin::class);
+    }
+}